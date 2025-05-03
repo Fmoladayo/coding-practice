b4f897e28d3b7a3307ba58ac756d9555d7c15348
unknown <fmoladayo10@yahoo.com>
2025-05-02 22:08:21 -0700

    coding practice

---
+++ Untitled-1.js
@@ -0,0 +1,8 @@
+console.log("1. I start playing");
+
+setTimeout(() => {
+  console.log("2. The robot brings me my toy after 2 seconds");
+}, 2000); // 2000 milliseconds = 2 seconds
+
+console.log("3. I keep playing with blocks");
+ 
